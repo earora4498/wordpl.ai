53f2d2e37d98de6f2a8580908986323f079bf888
mattqemo <mattqemo@umich.edu>
2019-09-14 16:59:28 -0400

    added gui and player files

---
+++ player.py
@@ -0,0 +1,13 @@
+class Player:
+
+    def __init__(self, username_in, score_in, teammate_in): # instance variable unique to each instance
+        self.username = username_in
+        self.score = score_in
+        self.teammate = teammate_in
+        teammate_in.teammate = self
+
+
+    username
+    score
+    teammate
+
