b65023bcf2e324124240f01f12eae5ea53f36f5e
ear <ear@umich.edu>
2019-09-15 11:19:03 -0400

    updated up/down game and added speech to text file

--- game.py
+++ game.py
@@ -18,8 +18,8 @@ if gameChoice == '1':
 	text = file.read()
 	myList = text.split()
 
-	first500 = myList[0:499]
-	words = set(first500)
+	first1000 = myList[0:999]
+	words = set(first1000)
 
 	#get a phrase from the user
 	phraseInput = raw_input("Enter a phrase: ")
@@ -28,7 +28,7 @@ if gameChoice == '1':
 	#check if every word in phrase is also in the map
 	for word in phrase:
 		if (word not in words):
-			print("Oh no! The word \'" + word + "\' is not in the 500 most frequent words!")
+			print("Oh no! The word \'" + word + "\' is not in the 1000 most frequent words!")
 			sys.exit(0)
 
 	print("That's a valid phrase!")
@@ -58,4 +58,39 @@ elif gameChoice == '2':
 
 elif gameChoice == '3':
 #Up/Down
-	print("TODO")
+	
+	file = open("30000words.txt", "r")
+	text = file.read()
+	myList = text.split()
+	words = set(myList)
+
+	currentWord = random.choice(myList)
+
+	done = False
+
+	while (not done):
+		currValue = myList.index(currentWord)
+		direction = bool(random.getrandbits(1))
+		directionWord = "more" if direction else "less"
+		print("The current word is " + currentWord)
+
+		nextWord = raw_input("Say a word that is used " + directionWord + " frequently than the current word!\n")
+
+		if (nextWord in words):
+			value = myList.index(nextWord)
+			if (direction):
+				if (value > currValue):
+					done = True
+					print("Wrong!")
+				else:
+					currentWord = nextWord
+					currValue = value
+			else:
+				if (value < currValue):
+					done = True
+					print("Wrong!")
+				else:
+					currentWord = nextWord
+					currValue = value
+		else:
+			print("That word isn't in our dictionary - sorry!")
